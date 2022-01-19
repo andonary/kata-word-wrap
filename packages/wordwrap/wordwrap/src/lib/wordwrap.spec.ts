f7e1dea450a5f151f581efbdb69081678e0e7f33
Ando Nary <ando.nary@gmail.com>
2022-01-19 22:44:30 +0100

    word wrap

--- packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
@@ -1,7 +1,51 @@
 import { wordwrap } from './wordwrap';
 
-describe('wordwrapWordwrap', () => {
-  it('should work', () => {
-    expect(wordwrap()).toEqual('wordwrap-wordwrap');
+describe('wordwrap', () => {
+  it.each([
+    {
+      itMsg: 'it should wrap empty input',
+      word: '',
+      column: 0,
+      expectedResult: '',
+    },
+    {
+      itMsg: 'it should wrap nothing',
+      word: 'word',
+      column: 5,
+      expectedResult: 'word',
+    },
+    {
+      itMsg: 'it should wrap my only word',
+      word: 'word',
+      column: 3,
+      expectedResult: 'wor\nd',
+    },
+    {
+      itMsg: 'it should wrap my two words',
+      word: 'word word',
+      column: 3,
+      expectedResult: 'wor\nd\nwor\nd',
+    },
+    {
+      itMsg: 'it should wrap my two words',
+      word: 'word word',
+      column: 4,
+      expectedResult: 'word\nword',
+    },
+    {
+      itMsg: 'it should wrap my two words',
+      word: 'word word',
+      column: 6,
+      expectedResult: 'word\nword',
+    },
+  ])('$itMsg', (cases) => {
+    // Given
+    const { word, column, expectedResult } = cases;
+
+    // When
+    const wraped: string = wordwrap(word, column);
+
+    // Then
+    expect(wraped).toEqual(expectedResult);
   });
 });
--- packages/wordwrap/wordwrap/src/lib/wordwrap.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.ts
@@ -1,3 +1,17 @@
-export function wordwrap(): string {
-  return 'wordwrap-wordwrap';
+export function wordwrap(word: string, column: number): string {
+  if (word.length > column) {
+    const space = word.substring(0, column + 1).lastIndexOf(' ');
+    if (space > -1)
+      return (
+        word.substring(0, space) +
+        '\n' +
+        wordwrap(word.substring(space + 1), column)
+      );
+    return (
+      word.substring(0, column) +
+      '\n' +
+      wordwrap(word.substring(column), column)
+    );
+  }
+  return word;
 }
