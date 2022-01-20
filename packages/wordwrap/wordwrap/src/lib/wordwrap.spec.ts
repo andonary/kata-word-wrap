ed06c542cade3913204c25f19b06a0b88b7d012c
Ando Nary <ando.nary@gmail.com>
2022-01-20 22:56:54 +0100

    about 40 min

--- packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
@@ -1,7 +1,27 @@
 import { wordwrap } from './wordwrap';
 
-describe('wordwrapWordwrap', () => {
-  it('should work', () => {
-    expect(wordwrap()).toEqual('wordwrap-wordwrap');
+describe('wordwrap', () => {
+  function assertWordWrap(textToWrap: string, col: number, result: string) {
+    expect(wordwrap(textToWrap, col)).toEqual(result);
+  }
+
+  test('it should return empty string', async () => {
+    assertWordWrap('', 9, '');
+  });
+
+  test('it should return word for a large column', async () => {
+    assertWordWrap('word', 9, 'word');
+  });
+
+  test('it should wrap my only word', async () => {
+    assertWordWrap('wordwo', 3, 'wor\ndwo');
+    assertWordWrap('wordword', 3, 'wor\ndwo\nrd');
+    assertWordWrap('wordwordword', 3, 'wor\ndwo\nrdw\nord');
+  });
+
+  test('it should wrap my two words', async () => {
+    assertWordWrap('word word', 5, 'word\nword');
+    assertWordWrap('word word', 6, 'word\nword');
+    assertWordWrap('word word', 4, 'word\nword');
   });
 });
--- packages/wordwrap/wordwrap/src/lib/wordwrap.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.ts
@@ -1,3 +1,42 @@
-export function wordwrap(): string {
-  return 'wordwrap-wordwrap';
+function isColStopAtSpace(textToWrap: string, columns: number) {
+  return textToWrap.charAt(columns - 1) === ' ';
+}
+
+function isColStopAfterSpace(textToWrap: string, columns: number) {
+  return textToWrap.charAt(columns - 2) === ' ';
+}
+
+function isColStopBeforeSpace(textToWrap: string, columns: number) {
+  return textToWrap.charAt(columns) === ' ';
+}
+
+function splitIntoParts(textToWrap: string, columns: number, jump = false) {
+  let firstPart = textToWrap.substring(0, columns);
+  let secondPart = textToWrap.substring(jump ? columns + 1 : columns);
+  return { firstPart, secondPart };
+}
+
+export function wordwrap(textToWrap: string, columns: number): string {
+  if (textToWrap.length > columns) {
+    let { firstPart, secondPart } = splitIntoParts(textToWrap, columns);
+    if (isColStopAtSpace(textToWrap, columns)) {
+      ({ firstPart, secondPart } = splitIntoParts(
+        textToWrap,
+        columns - 1,
+        true
+      ));
+    }
+    if (isColStopAfterSpace(textToWrap, columns)) {
+      ({ firstPart, secondPart } = splitIntoParts(
+        textToWrap,
+        columns - 2,
+        true
+      ));
+    }
+    if (isColStopBeforeSpace(textToWrap, columns)) {
+      ({ firstPart, secondPart } = splitIntoParts(textToWrap, columns, true));
+    }
+    return firstPart + '\n' + wordwrap(secondPart, columns);
+  }
+  return textToWrap;
 }
