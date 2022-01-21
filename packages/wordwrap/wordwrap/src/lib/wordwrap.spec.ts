92831a7b49566925c09bac2627dd286f69129363
Ando Nary <ando.nary@gmail.com>
2022-01-21 09:28:45 +0100

    27 min

--- packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
@@ -1,7 +1,19 @@
 import { wordwrap } from './wordwrap';
 
 describe('wordwrapWordwrap', () => {
-  it('should work', () => {
-    expect(wordwrap()).toEqual('wordwrap-wordwrap');
+  it('should not wrap when it is not required', () => {
+    expect(wordwrap('', 0)).toEqual('');
+    expect(wordwrap('word', 9)).toEqual('word');
+  });
+
+  it('should wrap my only word', () => {
+    expect(wordwrap('typescript', 6)).toEqual('typesc\nript');
+    expect(wordwrap('typescript', 4)).toEqual('type\nscri\npt');
+  });
+
+  it('should wrap my two words', () => {
+    expect(wordwrap('jean paul', 4)).toEqual('jean\npaul');
+    expect(wordwrap('jean paul', 5)).toEqual('jean\npaul');
+    expect(wordwrap('jean paul', 6)).toEqual('jean\npaul');
   });
 });
--- packages/wordwrap/wordwrap/src/lib/wordwrap.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.ts
@@ -1,3 +1,35 @@
-export function wordwrap(): string {
-  return 'wordwrap-wordwrap';
+function splitIntoParts(text: string, col: number, space = 0) {
+  let firstPart = text.substring(0, col + space);
+  let secondPart = text.substring(col);
+  return { firstPart, secondPart };
+}
+
+function hasCutAfterSpace(firstPart: string) {
+  return firstPart.charAt(firstPart.length - 2) === ' ';
+}
+
+function hasStopAtSpace(firstPart: string) {
+  return firstPart.endsWith(' ');
+}
+
+function hasStopBeforeSpace(firstPart: string) {
+  return firstPart.startsWith(' ');
+}
+
+export function wordwrap(text: string, col: number): string {
+  text = text.trim();
+  if (text.length > col) {
+    let { firstPart, secondPart } = splitIntoParts(text, col);
+    if (hasStopBeforeSpace(firstPart)) {
+      ({ firstPart, secondPart } = splitIntoParts(text, col));
+    }
+    if (hasStopAtSpace(firstPart)) {
+      ({ firstPart, secondPart } = splitIntoParts(text, col, -1));
+    }
+    if (hasCutAfterSpace(firstPart)) {
+      ({ firstPart, secondPart } = splitIntoParts(text, col - 1, -1));
+    }
+    return firstPart + '\n' + wordwrap(secondPart, col);
+  }
+  return text;
 }
