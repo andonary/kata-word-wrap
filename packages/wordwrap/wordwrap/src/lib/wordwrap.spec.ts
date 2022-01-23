1b12d9e83c5caaaa6ef29318841b94033e1b473f
Ando Nary <ando.nary@gmail.com>
2022-01-23 09:48:40 +0100

    several tries using tpp

--- packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
@@ -1,7 +1,22 @@
 import { wordwrap } from './wordwrap';
 
-describe('wordwrapWordwrap', () => {
-  it('should work', () => {
-    expect(wordwrap()).toEqual('wordwrap-wordwrap');
+describe('wordwrap', () => {
+  it('should wrap my word', () => {
+    expect(wordwrap('', 1)).toEqual('');
+    expect(wordwrap('x', 1)).toEqual('x');
+    expect(wordwrap('xx', 1)).toEqual('x\nx');
+    expect(wordwrap('xxx', 1)).toEqual('x\nx\nx');
+    expect(wordwrap('x x', 1)).toEqual('x\nx');
+    expect(wordwrap('x x x', 1)).toEqual('x\nx\nx');
+    expect(wordwrap('x x', 2)).toEqual('x\nx');
+    expect(wordwrap('jean paul', 6)).toEqual('jean\npaul');
+    expect(
+      wordwrap(
+        'jean alphonse lit un livre sur javascript et trouve cela cool',
+        5
+      )
+    ).toEqual(
+      'jean\nalpho\nnse\nlit\nun li\nvre\nsur\njavas\ncript\net tr\nouve\ncela\ncool'
+    );
   });
 });
--- packages/wordwrap/wordwrap/src/lib/wordwrap.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.ts
@@ -1,3 +1,18 @@
-export function wordwrap(): string {
-  return 'wordwrap-wordwrap';
+export function wordwrap(line: string, col: number): string {
+  if (line.length > col) {
+    let jump = 0;
+    let back = 0;
+    if (line.charAt(col) === ' ') jump = 1;
+    if (line.charAt(col - 1) === ' ') back = 1;
+    if (line.charAt(col - 2) === ' ') {
+      back = 2;
+      jump = -1;
+    }
+    return (
+      line.substring(0, col - back) +
+      '\n' +
+      wordwrap(line.substring(col + jump), col)
+    );
+  }
+  return line;
 }
