556b467a170d4fa8e04120ea5c04441c750b0739
Ando Nary <ando.nary@gmail.com>
2022-01-24 16:21:49 +0100

    video

--- packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.spec.ts
@@ -1,7 +1,25 @@
 import { wordwrap } from './wordwrap';
 
-describe('wordwrapWordwrap', () => {
-  it('should work', () => {
-    expect(wordwrap()).toEqual('wordwrap-wordwrap');
+describe('wordwrap', () => {
+  it('should wrap word', () => {
+    expect(wordwrap('', 1)).toEqual('');
+    expect(wordwrap('x', 1)).toEqual('x');
+    expect(wordwrap('xx', 1)).toEqual('x\nx');
+    expect(wordwrap('xxx', 1)).toEqual('x\nx\nx');
+  });
+
+  it('should wrap multiple words', () => {
+    expect(wordwrap('x x', 1)).toEqual('x\nx');
+    expect(wordwrap('jean paul', 5)).toEqual('jean\npaul');
+    expect(wordwrap('jean paul', 6)).toEqual('jean\npaul');
+
+    expect(
+      wordwrap(
+        'jean alphonse lit un livre sur javascript et trouve cela cool',
+        5
+      )
+    ).toEqual(
+      'jean\nalpho\nnse\nlit\nun li\nvre\nsur\njavas\ncript\net tr\nouve\ncela\ncool'
+    );
   });
 });
--- packages/wordwrap/wordwrap/src/lib/wordwrap.ts
+++ packages/wordwrap/wordwrap/src/lib/wordwrap.ts
@@ -1,3 +1,26 @@
-export function wordwrap(): string {
-  return 'wordwrap-wordwrap';
+function isStopAtSpace(line: string, col: number) {
+  return line.charAt(col) === ' ';
+}
+
+function isStopAfterSpace(line: string, col: number) {
+  return line.charAt(col - 1) === ' ';
+}
+
+function isStopAfterFirstLetter(line: string, col: number) {
+  return line.charAt(col - 2) === ' ';
+}
+
+export function wordwrap(line: string, col: number): string {
+  if (line.length > col) {
+    let firstPart = line.substring(0, col);
+    let secondPart = line.substring(col);
+    if (isStopAtSpace(line, col)) secondPart = line.substring(col + 1);
+    if (isStopAfterSpace(line, col)) firstPart = line.substring(0, col - 1);
+    if (isStopAfterFirstLetter(line, col)) {
+      firstPart = line.substring(0, col - 2);
+      secondPart = line.substring(col - 1);
+    }
+    return firstPart + '\n' + wordwrap(secondPart, col);
+  }
+  return line;
 }
